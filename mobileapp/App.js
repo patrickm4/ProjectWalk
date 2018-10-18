83fa73ab89f74bbd7c852122c87fc7f927e6a447
karizmo <32525686+karizmo@users.noreply.github.com>
2018-10-18 06:29:41 -0700

    first commit
    finished login

---
+++ mobileapp/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from "./comps/Login/Login"
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Login />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ mobileapp/comps/Login/Login.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          style={{width:200, height:200}}
+          source={require("./img/logo.png")}
+          />
+      <Text
+        style={[styles.walkRed, {padding: 20, marginBottom: 30, fontSize:50}]}
+        >WALK</Text>
+      <View style={styles.textInputBox}>
+        <TextInput
+          placeholder="Email"
+          style={{height: 40, width: 250, borderColor: 'gray', borderWidth: 1}}
+          underlineColorAndroid='transparent'
+          />
+      </View>
+      <View style={styles.textInputBox}>
+        <TextInput
+          placeholder="Password"
+          style={{height: 40, width: 250, borderColor: 'gray', borderWidth: 1}}
+          underlineColorAndroid='transparent'
+          />
+      </View>
+      <View style={styles.butView}>
+        <TouchableOpacity
+          style={styles.butBox}
+          >
+          <Text
+            style={[styles.textContainer, {color:'white',}]}
+            >
+            Login</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{position:'absolute', bottom:0}}
+        >
+        <Text>Dont have an account? Sign up</Text>
+      </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  bottomStick :{
+    //flex:1,
+    justifyContent: 'space-between'
+  },
+
+  butBox: {
+    backgroundColor: '#013b36',
+    alignItems: 'center',
+    width: 125,
+    height: 50,
+    borderRadius: 50
+  },
+
+  butView: {
+    display: "flex",
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+
+  walkRed: {
+    color: '#DB504A'
+  },
+
+  textInputBox: {
+    padding: 10
+  },
+
+  textContainer: {
+    lineHeight: 50,
+  },
+});
